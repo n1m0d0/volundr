837f06ab359a3be692ff23db006749e5bda78e67
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2022-04-04 15:57:46 -0400

    correcion en el json response de error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
             return response()->json([
                 'code' => 401,
                 'error' => 'Unauthorized'
-            ], 401);
+            ], 200);
         }
     }
 
