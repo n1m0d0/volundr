74fad2b649977216647c5a4081a7b4a41b219cce
n1m0d0 <33577927+n1m0d0@users.noreply.github.com>
2022-04-10 13:11:46 -0400

    arreglo en la consulta eventform para traer el formulario y sus perguntas y opciones

--- app/Http/Controllers/EventFormController.php
+++ app/Http/Controllers/EventFormController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Form;
+use App\Models\Option;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class EventFormController extends Controller
 {
     public function form(Form $form)
     {
-        $form->questions = $form->questions;
+        $questions = Question::where('form_id', $form->id)->where('status', 1)->orderBy('order', 'ASC')->get();
+        $form->questions = $questions;
         foreach ($form->questions as $question) {
-            $question->options = $question->options;
+            $options = Option::where('question_id', $question->id)->where('status', 1)->orderBy('order', 'ASC')->get();
+            $question->options = $options;
         }
-
+        
         return response()->json([
             'code' => 200,
             'data' => $form
